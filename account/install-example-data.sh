814a6fd9116969508c458270a3837cf85a53139f
Ivan Mincik <ivan.mincik@gmail.com>
2014-06-02 11:12:41 +0200

    account/install-example-data.sh: symlink data rather than copy to every account

--- account/install-example-data.sh
+++ account/install-example-data.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
-# Copy example data to each new user's Share directory
+# Symlink example data to each new user's Share directory
 
 
 case $GISLAB_ACCOUNT_ACTION in
 	add)
 		echo "Installing example data for user '$GISLAB_USER'."
-		cp -a /vagrant/user/data/natural-earth /storage/share/$GISLAB_USER/
-		chown -R $GISLAB_USER:labusers /storage/share/$GISLAB_USER/natural-earth
+		ln -s /storage/repository/data/natural-earth /storage/share/$GISLAB_USER/natural-earth
 		;;
 esac
 
